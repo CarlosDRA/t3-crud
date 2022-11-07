91000a8729d60ec0151060055bdaa134569dec29
Carlos Regalado <cd.regaladoaguirre@hotmail.com>
2022-11-07 11:09:23 -0600

    Added key to product

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,11 +1,10 @@
 import Product from "./Product";
-import Link from "next/link";
 
 const Products = ({ products } : {products:any}) => {
 
     return(
         <div className=" mt-20 px-40 grid grid-cols-3 gap-5 justify-items-center ">
-          {products.map((product:any) => <Product data={product} />)}
+          {products.map((product:any) => <Product data={product} key={product.name} />)}
         </div>
     )
 }
